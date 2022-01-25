42eb4fa07a8b8a4a8566ccb6d95f958ffe543ef3
Germán Moreno <germanclarinet03@gmail.com>
2022-01-25 17:53:11 -0500

    Taller 0 listo

---
+++ Taller0_Olimpicos_esqueleto/Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,14 @@
+package uniandes.dpoo.taller0.modificacion;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import  uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+
+public class Modificacion {
+
+	public static void main(String args[]) throws IOException, Exception  {
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+	}
+}
